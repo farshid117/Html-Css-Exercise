c2666e7cf0ab4e7e8d6760a8b1abdee6693d7667
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-09-11 18:22:34 +0330

    HTML/Css-Exercise --> flexbox-project: complete js source for Responsive menu

--- pages/13-project1-landingpage-pour-css/scripts/app.js
+++ pages/13-project1-landingpage-pour-css/scripts/app.js
@@ -1,14 +1,27 @@
-const menuIcon = document.querySelector(".menu")
-console.log("menuIcon: ", menuIcon);
+const menuList = document.querySelector(".menu")
+console.log("menuList: ", menuList);
 const hambergur = document.querySelector(".header_icon")
 const hambergurIcon = document.querySelector(".header_icon i")
+const liElm = document.querySelector(".menu__link")
 
-hambergur.addEventListener("click",()=>{
-    console.log("click")
-    menuIcon.classList.toggle("show")
-if(hambergurIcon.classList.contains("fa-bars")){
-    hambergurIcon.classList = "fa fa-times"
-}else{
-    hambergurIcon.classList = "fa fa-bars"
+function menuHandler(){
+   
+        console.log("click")
+        menuList.classList.toggle("show")
+        if (hambergurIcon.classList.contains("fa-bars")) {
+            hambergurIcon.classList = "fa fa-times"
+        } else {
+            hambergurIcon.classList = "fa fa-bars"
+        }
 }
+hambergur.addEventListener("click", (event)=>{
+    event.stopPropagation()
+    menuHandler()
 })
+menuList.addEventListener("click", menuHandler)
+ document.body.addEventListener("click", () => {
+    if (hambergurIcon.classList.contains("fa-times")) {
+        hambergurIcon.classList = "fa fa-bars"
+        menuList.classList.remove("show")
+    }
+}) 
